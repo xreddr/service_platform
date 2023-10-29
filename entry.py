72eb6a09c48c34ce157028ef8f9a6c69572ac587
xreddr <redwardhenion@gmail.com>
2023-10-29 15:14:08 -0400

    explicit origin on decorator login

--- entry.py
+++ entry.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = create_app()
 cors = CORS(app)
-# app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 if __name__ == "__main__":
     app.run(port=(os.environ.get("PORT", 8080)), host='0.0.0.0', debug=True)
