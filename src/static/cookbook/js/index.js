13e4f3ab8424148845726af05ce4d70b6a5bebb6
xreddr <redwardhenion@gmail.com>
2025-04-09 20:26:33 -0400

    Added JS index to calendar page.

---
+++ src/static/cookbook/js/index.js
@@ -0,0 +1,19 @@
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '-' + dd + '-' + yyyy;
+hashToday = '#' + today
+console.log(today)
+window.addEventListener('load', function() {
+    const element = document.getElementById(today);
+    console.log(element)
+    if (element) {
+      element.scrollIntoView();
+    }
+    this.window.scrollTo(0, element.offsetTop-200)
+    document.getElementById(element.id).style.border = "thick solid orange";
+  });
+
